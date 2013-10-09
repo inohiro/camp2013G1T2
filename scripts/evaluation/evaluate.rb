a54ea802a0abf9ce50d286204f1cb5528ff18bf7
Hiroyuki Inoue <mammymax@gmail.com>
2013-10-09 19:03:05 +0900

    add evaluation script

---
+++ scripts/evaluation/evaluate.rb
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+require 'csv'
+
+CORRECT_CSV = File.expand_path( "./../../data/fixed_user.csv" )
+TARGET_CSV  = File.expand_path( "./../66_result.csv" )
+
+def store_into_hash( csv )
+  hash = {}
+  CSV.foreach( csv ) do |raw|
+    user_id, location_id = raw[0], raw[1]
+    hash.store( user_id, location_id )
+  end
+  hash
+end
+
+correct = store_into_hash( CORRECT_CSV )
+target  = store_into_hash( TARGET_CSV  )
+
+# precision
+
+precision_result = []
+
+correct.each do |user_id, location_id|
+  target_location = target[user_id]
+  location_id == target_location ? precision_result << true : precision_result << false
+end
+
+correct_count = precision_result.count { |result| result == true }
+precision_score = correct_count.to_f / precision_result.size.to_f
+
+puts "Precision: #{precision_score}"
+
+# recall
+
+DEFAULT_UNKNOWN = 7305
+unknown_count = DEFAULT_UNKNOWN - target.count { |user_id, location_id| target[user_id] == UNKNOWN }
+recall_score = unknown_count.to_f / DEFAULT_UNKNOWN.to_f
+
+puts "Recall: #{recall_score}"
+
+# F-measure
+
+f_measure = 2 * precision_score * recall_score / ( precision_score + recall_score )
+puts "F-Measure: #{f_measure}"
